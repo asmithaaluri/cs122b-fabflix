eb78e26ea5167820968f7e3317fe1d5a79d53cf5
rebecca-123 <rebecca.notw@gmail.com>
2025-05-09 10:16:22 -0700

    add index for movie title and adjust query

--- create_table.sql
+++ create_table.sql
@@ -2,11 +2,15 @@ DROP DATABASE IF EXISTS moviedb;
 CREATE DATABASE moviedb;
 USE moviedb;
 
+CREATE TABLE custom_stopwords(value VARCHAR(30)) ENGINE = INNODB;
+SET GLOBAL innodb_ft_server_stopword_table = 'moviedb/custom_stopwords';
+
 CREATE TABLE movies (
     id VARCHAR(10) PRIMARY KEY,
     title VARCHAR(100) NOT NULL,
     year INTEGER NOT NULL,
-    director VARCHAR(100) NOT NULL
+    director VARCHAR(100) NOT NULL,
+    FULLTEXT idx (title)
 );
 
 CREATE TABLE stars (
